acfd3ea6c86d93db636b902ffaccba467404770b
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-22 15:18:53 -0400

    got link working for home and about

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar'
 import './App.css';
 import background from './assets/background.jpg'
+import Home from './components/Home'
+import About from './components/About'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -14,7 +16,7 @@ class App extends Component {
           <img className="background" src={background} alt=""></img>
           <Switch>
             <Route path="/" exact component={Home} />
-            <Route path='/signup' component={Signup} />
+            <Route path='/about' component={About} />
           </Switch>
         </div>
       </Router>);
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
-/* import { Link } from 'react-router-dom' */
-/* import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; */
+import { Link } from 'react-router-dom'
+
 function Navbar() {
     return (
         <div className="navbar-container">
             <ul className="navbar">
-                <li className="navbar-list"> Home </li>
-                <li className="navbar-list"> About </li>
+                <Link to='/'>
+                    <li className="navbar-list"> Home </li>
+                </Link>
+                <Link to='/about'>
+                    <li className="navbar-list"> About </li>
+                </Link>
             </ul>
         </div>
     )

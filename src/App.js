c49f5741f857b9f2526f676a4d1558473329dfdf
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-22 15:00:56 -0400

    added about.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar'
 import './App.css';
 import background from './assets/background.jpg'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   state = {}
   render() {
-    return (<div className="App">
-      <Navbar />
-      <img className="background" src={background} alt=""></img>
-    </div>);
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <img className="background" src={background} alt=""></img>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path='/signup' component={Signup} />
+          </Switch>
+        </div>
+      </Router>);
   }
 }
 
 export default App;
+
+

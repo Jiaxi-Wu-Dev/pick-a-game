995a2c07d7116888c5feaa5b97467e6afdb30933
Jiaxi <dev.jiaxiwu@gmail.com>
2020-04-23 19:45:47 -0400

    trying to filter and return first player games

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -21,7 +21,7 @@ class Sidebar extends Component {
     }; */
 
     async componentDidMount(){
-        const response = await axios.get("https://api.rawg.io/api/games?page_size=10")
+        const response = await axios.get("https://api.rawg.io/api/games?tags=singleplayer")
             .then(res => {
                 console.log(res)
                 this.setState({

b53af005cce8668db6b25f0cc346d2f4b7696d2f
Jiaxi <dev.jiaxiwu@gmail.com>
2020-04-24 00:07:40 -0400

    oneplayer.js

--- src/components/Oneplayer.js
+++ src/components/Oneplayer.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 const Oneplayer = () => {
+    async componentDidMount(){
+        const response = await axios.get("https://api.rawg.io/api/games?page_size=10&tag=31")
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    games: res.data.results
+                })
+
+            })
+            .catch(error => {
+                console.log('error:', error);
+            })
+        console.log(response)
+    } 
     return(<div>
-        
+
     </div>)
 }
+
+export default Oneplayer 

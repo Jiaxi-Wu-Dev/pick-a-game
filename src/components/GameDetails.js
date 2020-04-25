068bfdb12d5f01f09f7028ddd1b6a179e6784c8d
Jiaxi <dev.jiaxiwu@gmail.com>
2020-04-24 22:21:44 -0400

    gamedetails fetchitem

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -2,12 +2,15 @@ import React, {useState, useEffect} from 'react';
 
 
 function Game() {
-    useEffect(() => {}, []);
+    useEffect(() => {
+        fetchItem();
+    }, []);
 
     const [item, setItem] = useState({});
 
     const fetchItem = async () => {
         const fetchItem = await fetch (`https://api.rawg.io/api/games?id=3498`);
+        
         const item = await fetchItem.json();
 
         console.log(item);
